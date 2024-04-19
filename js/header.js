93b9aaa559179328e6c62f7661f8f96e573b42a2
imthaivu <imquocthai@gmail.com>
2024-04-19 19:46:04 +0700

    btn tang giam

--- js/header.js
+++ js/header.js
@@ -1,42 +1,58 @@
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector(".navbar");
 
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    userItem.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  userItem.classList.remove("active");
+  cartItem.classList.remove("active");
+};
 
-let searchForm = document.querySelector('.search-form');
+let searchForm = document.querySelector(".search-form");
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
-    userItem.classList.remove('active');
-}
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+  userItem.classList.remove("active");
+};
 
-let cartItem = document.querySelector('.cart-items-container');
+let cartItem = document.querySelector(".cart-items-container");
 
-document.querySelector('#cart-btn').onclick = () =>{
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    userItem.classList.remove('active');
-}
+document.querySelector("#cart-btn").onclick = () => {
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  userItem.classList.remove("active");
+};
 
-let userItem = document.querySelector('.user-content');
+let userItem = document.querySelector(".user-content");
 
-document.querySelector('#user-btn').onclick = () =>{
-    userItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
+document.querySelector("#user-btn").onclick = () => {
+  userItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
 
 window.onscroll = () => {
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-    userItem.classList.remove('active');
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+  userItem.classList.remove("active");
+};
+
+function increaseQuantity() {
+  var inputElement = document.getElementById("quantityInput");
+  var currentValue = parseInt(inputElement.value);
+  inputElement.value = currentValue + 1;
+}
+
+function decreaseQuantity() {
+  var inputElement = document.getElementById("quantityInput");
+  var currentValue = parseInt(inputElement.value);
+
+  // Giảm giá trị nếu giá trị hiện tại lớn hơn 1
+  if (currentValue > 1) {
+    inputElement.value = currentValue - 1;
+  }
 }
